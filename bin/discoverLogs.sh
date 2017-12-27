7deea881f020d82d93989ab502075cf4b0a5214c
Nick Hills <nick.hills@gmail.com>
2017-12-27 13:49:24 +0000

    Update discoverLogs.sh

--- bin/discoverLogs.sh
+++ bin/discoverLogs.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-
 find /  -type f -name "*.log" 2>/dev/null -print0 | while IFS= read -r -d $'\0' line; do
         echo $(date +%Y%m%dT%H%M%S%Z) " hostName="$(hostname) " foundPath=\""$line"\" os=\""$(uname)"\""
 done
